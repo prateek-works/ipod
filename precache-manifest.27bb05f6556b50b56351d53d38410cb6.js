2e074676533068be0a047b8554613573597e8aea
prateek <prateekchauhan518@gmail.com>
2020-10-08 21:37:18 +0530

    Updates

---
+++ precache-manifest.27bb05f6556b50b56351d53d38410cb6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e78a84d2ee571bbf66a",
+    "url": "/ipod/static/css/main.5ce83d0b.chunk.css"
+  },
+  {
+    "revision": "1e78a84d2ee571bbf66a",
+    "url": "/ipod/static/js/main.a13e6c82.chunk.js"
+  },
+  {
+    "revision": "4319e8b4e94411c764ab",
+    "url": "/ipod/static/js/runtime~main.f1f92253.js"
+  },
+  {
+    "revision": "cdd86fec1e05942d55cd",
+    "url": "/ipod/static/js/2.fcbe8b51.chunk.js"
+  },
+  {
+    "revision": "099b65e8ec77b7c6657ecbc6817d5013",
+    "url": "/ipod/static/media/games.099b65e8.png"
+  },
+  {
+    "revision": "d81f51752ad96843efe37bce59d979e3",
+    "url": "/ipod/static/media/games2.d81f5175.png"
+  },
+  {
+    "revision": "957bf02520e09934b5d8114e04780e82",
+    "url": "/ipod/index.html"
+  }
+];
